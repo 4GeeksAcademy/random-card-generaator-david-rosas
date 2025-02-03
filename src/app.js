a7b031d7173401f351bb600c2462072ad4854c1d
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2025-02-03 00:39:29 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // de aca cambiamos la information en el html
+  const primeraPinta = document.querySelector("#PintaArriba");
+  const segundaPinta = document.querySelector("#PintaAbajo");
+  const valorCarta = document.querySelector("#Valor");
+
+  //Esta es la nueva informacion}
+
+  const pintas = ["♠", "♦", "♣", "♥"];
+  const valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  //generamos el valor random
+
+  function informacionRandom(list) {
+    let randomValue = Math.floor(Math.random() * list.length);
+    return list[randomValue];
+  }
+
+  let randomPinta = informacionRandom(pintas);
+  let randomValor = informacionRandom(valores);
+  primeraPinta.innerHTML = randomPinta;
+  segundaPinta.innerHTML = randomPinta;
+  valorCarta.innerHTML = randomValor;
+  if (randomPinta == "♦" || randomPinta == "♥");
+  primeraPinta.style.color = "red";
+  segundaPinta.style.color = "red";
 };
